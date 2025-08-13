2a33d92c7d195d9b830ce0e2abd6896074e8a259
InfinityBurrito <maxjw1207@gmail.com>
2025-08-12 20:16:37 -0700

    controller

--- get-the-apple/data/scripts/player.gd
+++ get-the-apple/data/scripts/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody3D
 
 @export var speed := 100
-@export var gravity := -100
-@export var jumpHeight := 10
+@export var gravity := -50
+@export var jumpHeight := 20
 @onready var neck: Node3D = $neck
 @onready var cam: Camera3D = $neck/Camera3D
 @export var sens = -0.01
+var jumpBuffer := 0.0
+var coyoteTime := 0.0
+var friction := 0.8
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -24,6 +27,20 @@ func _physics_process(delta: float) -> void:
 	var direction = (neck.transform.basis * transform.basis * Vector3(InputVec.x, 0, InputVec.y)).normalized()
 	if direction:
 		velocity += direction*speed*delta
-	velocity *= 0.8
+	velocity *= Vector3(friction,1,friction)
 	move_and_slide()
+	if Input.is_action_just_pressed("jump"):
+		jumpBuffer = 5
+	if is_on_floor():
+		coyoteTime = 5
+	if jumpBuffer>0 and coyoteTime>0:
+		jump()
+		jumpBuffer = 0
+		coyoteTime = 0
+	jumpBuffer -= delta
+	coyoteTime -= delta
+	pass
+
+func jump():
+	velocity.y = jumpHeight
 	pass
